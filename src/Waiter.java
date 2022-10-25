8d81c975cc215715c3e93b7f003991689bb63222
thientranreal <116498615+thientranreal@users.noreply.github.com>
2022-10-25 13:09:36 +0700

    Update Waiter.java
    override method calSalary

--- src/Waiter.java
+++ src/Waiter.java
@@ -1,6 +1,13 @@
 public class Waiter extends Employee implements CalculateSalary{
     @Override
     public int calSalary(int working_days) {
+        int salary = getWage() * working_days;
+        if (getType().compareToIgnoreCase("full-time") == 0) {
+            return salary + getWage();
+        }
+        if (getType().compareToIgnoreCase("part-time") == 0) {
+            return salary;
+        }
         return 0;
     }
 }
