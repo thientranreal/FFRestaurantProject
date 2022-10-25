fbf646fcbf63066fb65c83abcc2651acd55a1be8
thientranreal <116498615+thientranreal@users.noreply.github.com>
2022-10-25 13:12:54 +0700

    update interface cleaningstaff, guard

--- src/Guard.java
+++ src/Guard.java
@@ -1,6 +1,6 @@
 public class Guard extends Employee implements CalculateSalary{
     @Override
     public int calSalary(int working_days) {
-        return 0;
+        return getWage() * working_days;
     }
 }
